5c3c34d058128fc06225cf4a916708977f9d579f
kimdogbe <kimdogbe@gmail.com>
2024-10-15 16:39:16 +0100

    Add js functions to create book elements

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const library = [
+  new Book("Harry Potter", "JK Rowling", 354, true),
+  new Book("Tomorrow, tomorrow and tomorrow", "RS James", 347, true)
+]
+
 function Book(title, author, pages, read){
   this.title = title;
   this.author = author;
@@ -8,3 +13,29 @@ function Book(title, author, pages, read){
     return `${this.title} by ${this.author} is ${this.pages} long. ${this.read ? "Has been read" : "Not read yet"}`;
   }
 }
+
+function addBookToLibrary(){
+  return true;
+}
+
+function createBookCards(book){
+  let bookDiv = document.createElement("div");
+  bookDiv.id = "book1";
+
+  let bookInfo = document.createElement("p");
+  bookInfo.innerHTML = book.info();
+
+  bookDiv.appendChild(bookInfo);
+  console.log(bookDiv);
+
+  document.getElementById("shelf").appendChild(bookDiv);
+  
+}
+
+function displayBooks(books) {
+  for (const book of books){
+    createBookCards(book);
+  }
+}
+
+displayBooks(library)
