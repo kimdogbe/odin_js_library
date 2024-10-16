b42396f2994fb307b6eb77f9eaf975dadc4f662c
kimdogbe <kimdogbe@gmail.com>
2024-10-16 15:01:39 +0100

    Add delete book button and logic

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const newBookForm = document.querySelector("dialog");
 const addBookBtn = document.querySelector("dialog + button");
 const closeModal = document.querySelector("dialog button");
 const submitBook = document.querySelector("dialog #book-submit");
+const shelf = document.querySelector("#shelf");
+
+const newBookTitle = document.querySelector("#book-name");
+const newBookAuthor = document.querySelector("#book-author");
+const newBookPages = document.querySelector("#book-page-count");
+const newBookRead = document.querySelector("#book-complete");
 
 function Book(title, author, pages, read){
   this.title = title;
@@ -19,31 +25,41 @@ function Book(title, author, pages, read){
   }
 }
 
-function addBookToLibrary(e){
-  // const bookInfo = document.querySelector("#book-info");
-  // const formData = new FormData(bookInfo);
-  console.log(`book submitted ${e}`);
+function addBookToLibrary(newBook){
+  library.push(newBook);
 }
 
-function createBookCards(book){
+function createBookCards(book, index){
   let bookDiv = document.createElement("div");
-  bookDiv.id = "book1";
+  bookDiv.id = `book-${index}`;
 
   let bookInfo = document.createElement("p");
   bookInfo.innerHTML = book.info();
 
+  let bookDeleteBtn = document.createElement("button");
+  bookDeleteBtn.innerHTML = 'Delete'
+
   bookDiv.appendChild(bookInfo);
+  bookDiv.appendChild(bookDeleteBtn);
   console.log(bookDiv);
 
-  document.getElementById("shelf").appendChild(bookDiv);
-  
+  shelf.appendChild(bookDiv);
 }
 
 function displayBooks(books) {
-  for (const book of books){
-    createBookCards(book);
+  shelf.innerHTML = "";
+  for (const [index, book] of books.entries()){
+    createBookCards(book, index);
   }
 }
+
+addEventListener("click", (event) => {
+  if (event.target.innerHTML === "Delete") {
+    let bookIndex = event.target.parentElement.id.split("-")[1]
+    library.splice(bookIndex, 1);
+    displayBooks(library);
+  }
+})
 
 addBookBtn.addEventListener("click", () => {
   newBookForm.showModal();
@@ -54,7 +70,18 @@ closeModal.addEventListener("click", () => {
 });
 
 submitBook.addEventListener("click", (event) => {
-  addBookToLibrary(event);
+  event.preventDefault();
+  
+  let newBook = new Book(
+    newBookTitle.value,
+    newBookAuthor.value,
+    newBookPages.value,
+    newBookRead.checked
+  );
+
+  addBookToLibrary(newBook);
+  displayBooks(library);
+  newBookForm.close();
 });
 
-displayBooks(library)
+displayBooks(library);
